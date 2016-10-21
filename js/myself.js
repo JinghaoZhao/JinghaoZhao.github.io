0e3ba18537dc702346c9e5be3e4340af9e54679f
Vincent <zhaojinghao1996@gmail.com>
2016-10-21 09:18:36 +0800

    Version 1.2 --change a few errors.

---
+++ js/myself.js
@@ -0,0 +1,17 @@
+/**
+ * Created by MyPC on 2016/4/26.
+ */
+
+var canvas = document.getElementById("mycanvas").getContext("2d") ;
+
+canvas.shadowOffsetX = 100;
+canvas.shadowOffsetY = 100;
+canvas.shadowBlur = 10;
+canvas.shadowColor = 'blue';
+canvas.fillStyle = 'blue';
+canvas.strokeStyle = '#09c';
+canvas.lineWidth = 5;
+canvas.fillRect(0,0,100,100);
+canvas.clearRect(25,25,50,50);
+canvas.strokeRect(25,25,50,50);
+
